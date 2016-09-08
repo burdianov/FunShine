f273cb82e35795c9d73cd16e4b623444dc5651c6
Leo Burdianov <leo.burdianov@gmail.com>
2016-09-08 21:08:34 +0400

    Add base URL and API key constants

--- app/src/main/java/com/testography/funshine/WeatherActivity.java
+++ app/src/main/java/com/testography/funshine/WeatherActivity.java
@@ -5,9 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
+    final String URL_COORD = "/?lat=";
+    final String URL_API_KEY = "&APPID=15694f7dfc46ad9ed54037392ddf05be";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+
     }
 }
